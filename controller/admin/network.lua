c59b3a4b363adac97d84156f16be4de67c572c98
sinalpha <hihahajun@gmail.com>
2015-10-29 18:14:07 +0800

    custom nav bar

---
+++ controller/admin/network.lua
@@ -0,0 +1,20 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2008 Steven Barth <steven@midlink.org>
+Copyright 2011 Jo-Philipp Wich <xm@subsignal.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+]]--
+
+module("luci.controller.admin.network", package.seeall)
+
+function index()
+	entry({"admin", "network"}, template("admin_network/index"), _("路由器设置"), 60).index = true
+end
+
