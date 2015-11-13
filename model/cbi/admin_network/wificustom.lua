66d7deadead1bef56e83cb820f5ad148cc57d4dc
sinalpha <hihahajun@gmail.com>
2015-11-13 14:50:42 +0800

    refine the wifi form

--- model/cbi/admin_network/wificustom.lua
+++ model/cbi/admin_network/wificustom.lua
@@ -1,5 +1,7 @@
-m = Map("system", translate("WiFi Password"),
-	translate("Changes the WIFI password or SSID, you can change each other independently.  Password length must be greater than 8"))
+so = luci.sys.exec("uci get wireless.@wifi-iface[0].ssid")
+po = luci.sys.exec("uci get wireless.@wifi-iface[0].key")
+m = Map("system", translate("current  WiFi name is:  " .. so .. "  <br /> current password is:  " .. po),
+        translate("Changes the WIFI password or SSID, you can change each other independently.<br />Password length must be greater than 8"))
 
 s = m:section(TypedSection, "_dummy", "")
 s.addremove = false
@@ -7,50 +9,41 @@ s.anonymous = true
 
 sd = s:option(Value, "ssid", translate("WiFi name (SSID)"))
 
-placehold = s:option(Flag,"enable",translate("Change wifi Name and Password together"))
+s_button = s:option(Button,"_btn",translate("Change Name"))
 
 pw1 = s:option(Value, "pw1", translate("Password"))
 pw1.password = true
 
-pw2 = s:option(Value, "pw2", translate("Password Confirmation"))
-pw2.password = true
+p_button = s:option(Button,"_btn_2",translate("Change  Password"))
+
 
 function s.cfgsections()
 	return { "_pass" }
 end
 
-function m.on_commit(map)
+function s_button.write()
 	local SSID = sd:formvalue("_pass")
-	local v1 = pw1:formvalue("_pass")
-	local v2 = pw2:formvalue("_pass")
-	local sok = 'not'
-	local pok = 'not'
-
-	if #SSID > 0 then
+	if SSID and #SSID > 0 then
 		luci.sys.exec("uci set wireless.@wifi-iface[0].ssid='" .. SSID .. "' && uci commit")
-		sok = 'ok'
+		m.message = translate("Success! SSID changed to: " .. SSID .. ". Click the save buttom to save the changes")
 	else
 		m.message = translate("SSID NOT changed!! SSID cannot be blank!")
-		if #v1 > 7 then
-			sok = 'ok'
-		end
 	end
+end
 
-	if v1 == v2 and #v1 >7 then
-		luci.sys.exec("uci set wireless.@wifi-iface[0].key='" .. v1 .. "' && uci commit")
-		pok = 'ok'
+function p_button.write()
+	local pd = pw1:formvalue("_pass")
+	if pd and #pd > 7 then
+		luci.sys.exec("uci set wireless.@wifi-iface[0].key='" .. pd .. "' && uci commit")
+		m.message = translate("Success! password changed to: " .. pd .. ". Click the save buttom to save the changes")
 	else
-		m.message = translate("Password not changed!\n Password LENGTH must be greater than 8!\n OR your can just leave it blank, changing the SSID only.")
-		if #v1 == 0 then
-			pok = 'ok'
-		end
+		m.message = translate("Password  NOT changed!! Password cannot be blank!  Password length must be greater than 8 ")
 	end
+end
 
-	if sok == 'ok'and pok == 'ok' then
-		m.message = translate("Password or SSID successfully changed!")
-		luci.sys.exec("/etc/init.d/network restart")
-	end
 
+function m.on_commit(map)
+	luci.sys.exec("/etc/init.d/network restart")
 end
 
 return m
